b73123c6d7018891e6293b3bb78c41bb9acda88b
Alejo Silva <alejosilvalau@Alejos-MBP.fibertel.com.ar>
2022-04-20 20:48:01 -0300

    first commit

---
+++ scripts/fund_and_withdraw.py
@@ -0,0 +1,23 @@
+from ast import withitem
+from brownie import FundMe
+from scripts.helpful_scripts import get_account
+
+
+def fund():
+    fund_me = FundMe[-1]
+    account = get_account()
+    entrance_fee = fund_me.getEntranceFee()
+    print(f"The current entry fee is {entrance_fee}")
+    print("Funding")
+    fund_me.fund({"from": account, "value": entrance_fee})
+
+
+def withraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    fund_me.withdraw({"from": account})
+
+
+def main():
+    fund()
+    withraw()
